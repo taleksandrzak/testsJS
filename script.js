5f267a226878281ccedd46306106e1fc60d8bb5a
Tomasz Aleksandrzak <taleksandrzak@zut.edu.pl>
2023-07-07 13:46:19 +0200

    learning JS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+const favColor = 'niebieski'
+const favMeal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+const firstName2 = 'Ania'
+let age2 = 24
+const favColor2 = 'czerwony'
