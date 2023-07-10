2cc2d6f189423a3a78f1a4ba614e053504263553
Tomasz Aleksandrzak <t.aleksandrzak@o2.pl>
2023-07-10 17:32:46 +0200

    lesson 31

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-const firstName = 'Tomek'
-let age = 12
-age = 18
- 
-const favColor = 'niebieski'
-const favMeal = 'schabowy'
- 
-let currentCar
-currentCar = 'Audi'
- 
-const firstName2 = 'Ania'
-let age2 = 24
-const favColor2 = 'czerwony'
+const colors = ['red','green','blue'];
+console.log(colors);
